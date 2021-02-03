bb0ab849facc899ac053ccce49e34c5ddfa30280
PalMassimo <massimo.palmisano02@gmail.com>
2021-02-03 14:20:15 +0100

    Python script written in a pythonic way

--- Python/reverse_dictionary.py
+++ Python/reverse_dictionary.py
@@ -1,28 +1,18 @@
 #!/usr/bin/env python3
-dictionary  = {"a": [1, 2, 3], "b": [45, 6], "c": [2, 45]}
+dictionary = {"a": [1, 2, 3], "b": [45, 6], "c": [2, 45]}
 
 print("The original dictionary is ", dictionary)
 
 rd = dict()
 my_set = set()
 
-for value in dictionary.values():
-	my_set = my_set.union(set(value))
-
-for elem in my_set:
-	rd[elem] = list()
-
-for elem in rd.keys():
-	for key, value in dictionary.items():
-		for list_elem in value:
-			if(elem == list_elem):
-				#print("rd[elem]", elem, rd[elem])
-				rd[elem].append(key)
+for elem in set([value for values_list in dictionary.values() for value in values_list]):
+	rd[elem] = [key for key, value in dictionary.items() for list_elem in value if elem == list_elem]
 
 print("The final reversed dictionary ", rd)
 rd2 = {1: ["a"], 2: ["a", "c"], 3: ["a"], 6: ["b"], 45: ["b", "c"]}
 
-if (rd==rd2): 
-	print("The computed reversed dictionary is correct")
-else: 
-	print("something went wrong")
+if (rd == rd2):
+    print("The computed reversed dictionary is correct")
+else:
+    print("something went wrong: ", rd)
